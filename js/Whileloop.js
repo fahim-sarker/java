ab07932ba9f1883f889798642f43779477be6b1e
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2025-01-11 07:24:31 +0600

    loop testing

--- js/Whileloop.js
+++ js/Whileloop.js
@@ -7,3 +7,20 @@ function changeBackgroundColor() {
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 }
+
+
+const radius = 10;
+
+for (let y = -radius; y <= radius; y++) {
+  let row = '';
+
+  for (let x = -radius; x <= radius; x++) {
+    const distance = Math.sqrt(x * x + y * y);
+    if (Math.abs(distance - radius) < 0.5) {
+      row += '*';
+    } else {
+      row += ' ';
+    }
+  }
+  console.log(row);
+}
